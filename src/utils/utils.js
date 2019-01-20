73a60d44e5e8ccc1c046d0a52e1a233d87f89e69
bibi <wzj2584454@126.com>
2019-01-20 18:58:10 +0800

    feature: add time formatter
    Signed-off-by: bibi <wzj2584454@126.com>

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,19 @@
-export function test(num) {
-  alert(num)
+/**
+ * 时间戳标准化
+ * @param {Number | String} time 时间戳
+ * @param {String} gap 间隔符
+ * @returns {String} 标准日期时间
+ */
+export const dateFormatter = (time, gap = '/') => {
+  if (!time) throw 'the argument time need to be passed in'
+  if (typeof gap !== 'string') throw 'not a validate gap format'
+
+  const timeFormat = typeof time === 'number'
+    ? time.toString().length === 13 ? time : time * 1000
+    : throw 'not a validate time format'
+
+  if (timeFormat) {
+    const date = new Date(timeFormat)
+    return `${date.getFullYear()}${gap}${date.getMonth() + 1}${gap}${date.getDate()}`
+  }
 }
